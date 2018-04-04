7137ddca46714e5d556784c36c2e3c497e426918
siditom <tomer.cd@gmail.com>
2018-04-04 15:27:26 +0300

    Did not add the files in the last commit.

--- scripts/submitCA.sh
+++ scripts/submitCA.sh
@@ -53,14 +53,15 @@ logs=${currDir}/logs
 
 params=`basename $paramsPath | sed 's/.m//g'`
 
+rnd=$RANDOM
 for i in $(seq 1 $mul)
 do
-  rnd=$RANDOM
   job_name=Con.${params}.$rnd
   echo ${job_name} >> tmp.$$
   ${APP_PATH}/scripts/qWait.sh $MJobs $tIntervalConfig
   echo "qsub -l h_vmem=6G -S /bin/sh -cwd -V -hold_jid $JobDependencies -N $job_name -q $servers -o $logs/$job_name.log -e $logs/$job_name.error ${APP_PATH}/scripts/execMatlab.sh $currDir/Configurations createConfigs(num2str($rnd) $CVFlag)" 
   qsub -l h_vmem=6G -S /bin/sh -cwd -V -hold_jid $JobDependencies -N $job_name -q $servers -o $logs/$job_name.log -e $logs/$job_name.error ${APP_PATH}/scripts/execMatlab.sh $currDir/Configurations "createConfigs(num2str($rnd) $CVFlag)" 
+  rnd=$((${rnd} + 1))
 done 
 
 ${APP_PATH}/scripts/qWait.sh $MJobs $tIntervalConfig 
--- start.sh
+++ start.sh
@@ -4,12 +4,13 @@ function print_help(){
   echo "USAGE: meshiMC [OPTION]"
   echo "[-ca]  Create configuration array."
   echo "[-vd]  Create validation data."
-  echo "[-cv]  Cross-validation - Not Supported Yet."
+  echo "[-cv]  Cross-validation."
+  echo "[-cvn]  Cross-validation - With existing partition file."
 }
 
+APP_PATH=/home/cluster/users/siditom/code/meshiMC
 
-
-source /home/cluster/users/siditom/code/meshiMC/data/setEnv.sh
+source $APP_PATH/data/setEnv.sh
 
 if [ $# -lt 1 ] || [ "$1" = "-help" ]; then
   print_help
@@ -17,7 +18,8 @@ if [ $# -lt 1 ] || [ "$1" = "-help" ]; then
 fi
 
 if [ "$1" = "-ca" ];then
-  $APP_PATH/scripts/submitCA.sh 3 params/pv_test.m test
+#  $APP_PATH/scripts/submitCA.sh 3 params/pv_test.m test
+  $APP_PATH/scripts/submitCA.sh ${@:2}
   exit
 fi
 
@@ -35,5 +37,9 @@ if [ "$1" = "-cv" ] && [ $# -ge 4 ];then
   exit
 fi
 
+if [ "$1" = "-cvn" ] && [ $# -ge 4 ];then
+  $APP_PATH/scripts/submitCV.sh $2 $3 ${@:4}
+  exit
+fi
 
 print_help
